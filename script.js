324e682f5d4fa9432e1be4cf51367ed447dbeaae
hiroaki-ohnaka <110279990+hiroaki-ohnaka@users.noreply.github.com>
2023-10-30 13:47:34 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,79 @@
 // JavaScript
 
-const main = document.querySelector('#main-image');
-const thumbs = document.querySelectorAll('.thumb');
-const fruits = ['apple','banana','orange'];
+// フェード
+const fade = document.querySelector('.fade');
 
-//console.log(main);
-//console.log(thumbs);
-const text = document.querySelector('#caption');
+const fadeKeyframes = {
+  opacity: [0, 1],
+}
 
-thumbs.forEach(function(item, index) {
-    item.onmouseover = function() {
-      //console.log(this.dataset.image);
-      main.src = this.dataset.image;
-      text.textContent = this.title;
-    }
-  });
+const fadeOptions = {
+  duration: 3000,
+  easing: 'ease',
+  fill: 'forwards',
+}
+
+fade.animate(fadeKeyframes, fadeOptions);
+
+// 回転
+const spin = document.querySelector('.spin');
+
+const spinKeyframes = {
+  rotate: ['x 360deg','x 0deg'],
+  opacity: [0, 1],
+}
+
+const spinOptions = {
+  duration: 2000,
+  easing: 'ease',
+}
+
+spin.animate(spinKeyframes, spinOptions);
+
+// 色が変わる
+const color = document.querySelector('.color');
+
+const colorKeyframes = {
+  backgroundColor: ['orange ','yellow ','lightgreen','skyblue'],
+}
+
+const colorOptions = {
+  duration: 5000,
+  direction: 'alternate',
+  iterations: Infinity,
+}
+
+color.animate(colorKeyframes,colorOptions);
+
+
+// 背景が延びる
+const line = document.querySelector('.line');
+
+const lineKeyframes = {
+  width: ['0','100%'],
+  color: ['transparent','#FFF'],
+}
+
+const lineOptions = {
+  duration: 2000,
+  fill: 'forwards',
+  easing:'ease-out',
+}
+
+line.animate(lineKeyframes,lineOptions);
+
+
+// 文字のスライドイン
+const slide = document.querySelector('.slide-in');
+
+const slideKeyframes = {
+  opacity: [0, 1],
+  translate: ['0 50px', 0],
+};
+
+const slideOptions = {
+  duration: 1000,
+  fill: 'forwards',
+};
+
+slide.animate(slideKeyframes,slideOptions);
